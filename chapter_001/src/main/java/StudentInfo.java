8f3e69ea84429ca8046d6ca60ef453e36a46df72
artem-bazanov <atremjava@gmail.com>
2019-12-20 11:42:23 +0300

    3. Коммиты на решения задач.[#18069]

---
+++ chapter_001/src/main/java/StudentInfo.java
@@ -0,0 +1,7 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Artem Bazanov");
+        System.out.println("21.11.19");
+
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,31 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static int rubleToEuro(int value) {
+        return value / 70;
+    }
+
+    public static int rubleToDollar(int value) {
+        return value / 60;
+    }
+
+    public static int dollarToRuble(int value) {
+        return 60 * value;
+    }
+
+    public static int euroToRuble(int value) {
+        return 70 * value;
+    }
+
+    public static void main(String[] args) {
+        int euro = rubleToEuro(140);
+        int dollar = rubleToDollar (120);
+        int rubleDollar = dollarToRuble(2) ;
+        int rubleEuro = euroToRuble(2);
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("120 rubles are " + dollar + " dollar.");
+        System.out.println("2 dollar are " + rubleDollar + " rubles.");
+        System.out.println("2 euro are " + rubleEuro + " rubles.");
+
+    }
+}
