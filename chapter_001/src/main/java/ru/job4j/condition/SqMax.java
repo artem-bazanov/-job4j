9ce534afcad2182ebf9a304074877bc0024cf874
artem-bazanov <atremjava@gmail.com>
2020-01-29 14:03:55 +0300

    4.5. Отладка программы в IDEA[#200843] (правки)

--- chapter_001/src/main/java/ru/job4j/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -3,16 +3,12 @@ package ru.job4j.condition;
 public class SqMax {
     public static int max(int first, int second, int third, int forth) {
         int result = forth;
-        if (first > second) {
-            if (first > third) {
-                if (first > forth) {
-                    result = first;
-                }
-            }
-        } else if (second > third) {
-            if (second > forth) {
+        if (first > second && first > third && first > forth) {
+             result = first;
+
+        } else if (second > third && second > forth) {
                 result = second;
-            }
+
         } else if (third > forth) {
             result = third;
         }
--- chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertThat;
 public class SqMaxTest {
     @Test
     public void whenFirstMax() {
-        int result = SqMax.max(5, 1, 2, 1);
+        int result = SqMax.max(5, 4, 2, 1);
         assertThat(result, is(5));
     }
 
@@ -19,13 +19,19 @@ public class SqMaxTest {
 
     @Test
     public void whenThirdMax() {
-        int result = SqMax.max(1, 4, 9, 1);
+        int result = SqMax.max(4, 1, 9, 2);
         assertThat(result, is(9));
     }
 
     @Test
     public void whenForthMax() {
-        int result = SqMax.max(1, 4, 7, 8);
-        assertThat(result, is(8));
+        int result = SqMax.max(1, 4, 7, 9);
+        assertThat(result, is(9));
+    }
+
+    @Test
+    public void whenThird2Max() {
+        int result = SqMax.max(6, 4, 7, 5);
+        assertThat(result, is(7));
     }
 }
