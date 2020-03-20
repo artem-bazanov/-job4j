da168632621b46d665d3bec4abd624f23c041dac
artem-bazanov <atremjava@gmail.com>
2020-03-20 13:19:16 +0300

    5.8. JavaDoc. Code convention[#200850]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculator;
 
+/**
+ * Class Calculator для вычисления арифметических операций + - * /
+ * @author Artem Bazanov (atremjava@gmail.com)
+ * since 12.01.2020
+ * @version 1.1
+ */
 public class Calculator {
+    /**
+     * @param first - первый аргумент
+     * @param second - второй аргумент
+     * @return result - результат
+     */
     public static void add(double first, double second) {
         double result =  first + second;
         System.out.println(first + " + " + second + " = " + result);
@@ -17,7 +28,10 @@ public class Calculator {
         double result = first - second;
         System.out.println(first + " - " + second + " = " + result);
     }
-
+    /**
+     * main
+     * @param args - args
+     */
     public static void main(String[] args) {
         add(2, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Artem Bazanov (atremjava@gmail.com)
+ * @version 200798
+ * @since 0.1
+ */
+package ru.job4j.calculator;
+
