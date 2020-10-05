c2019de89cb5b02d93e1b545f8bfb9e62b074285
artem-bazanov <atremjava@gmail.com>
2020-10-05 12:37:10 +0300

    2. Модель данных. [#201007]

---
+++ chapter_002/src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.pojo;
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Артем Базанов");
+        student.setGroup("1-РИ2020");
+        student.setAdmission(new Date());
+
+        System.out.println("Student name - " + student.getName() + System.lineSeparator() + "Group - " + student.getGroup() + System.lineSeparator() + "Receipt date - " + student.getAdmission());
+    }
+}
