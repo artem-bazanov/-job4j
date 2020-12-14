ffa32d2383036f5ae912b6fd27b01192133ad53d
artem-bazanov <atrembazanov@gmail.com>
2020-12-14 15:32:43 +0300

    1.1.1. Конструктор по умолчанию [#425290]

--- chapter_002/src/main/java/ru/job4j/oop/Fix.java
+++ chapter_002/src/main/java/ru/job4j/oop/Fix.java
@@ -1,7 +1,15 @@
 package ru.job4j.oop;
 
 public class Fix {
+
+    private String desc;
+
+    public Fix(String text) {
+        desc = text;
+    }
+
     public static void main(String[] args) {
-        Fix item = new Fix();
+        String issue = "Fix";
+        Fix item = new Fix(issue);
     }
 }
